3fb70214f3d6e7e3373acde998b6a3bb80c9fe3a
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-01-28 08:01:17 +0800

    Delete Compilation directory

--- Compilation/helloworld.asm
+++
@@ -1,8 +0,0 @@
-bdos    equ    0005H    ; BDOS entry point
-start:  mvi    c,9      ; BDOS function: output string
-        lxi    d,msg$   ; address of msg
-        call   bdos
-        ret             ; return to CCP
-
-msg$:   db    'Hello, world!$'
-end     start
